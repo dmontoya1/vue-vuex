e1dd591a74df8b809aa14a1de97afb2dabc01fb6
Daniel Montoya <dmontoya.lab@gmail.com>
2021-02-14 18:15:25 -0500

    Clase 39.. Primer State Global

--- src/store/index.js
+++ src/store/index.js
@@ -2,11 +2,16 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    titleApp: "Memes v2",
+    memes: []
   },
   mutations: {
   },
   actions: {
   },
   modules: {
+  },
+  getters: {
+
   }
 })
