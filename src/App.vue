26abdb1422e725457e28fc99be7cd36a6c63a97d
Daniel Montoya <dmontoya.lab@gmail.com>
2021-02-14 18:21:44 -0500

    Añadiendo Bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,13 @@
 <template>
-  <h1>{{ $store.state.titleApp }}</h1>
-  <h3>{{ titleApp }}</h3>
+  <div>
+    <button type="button" class="btn btn-primary">Primary</button>
+    <button type="button" class="btn btn-secondary">Secondary</button>
+  </div>
 </template>
 
 <script>
-import { useStore } from "vuex";
 export default {
   name: "App",
-  setup() {
-    const store = useStore();
-    console.log(store.state.titleApp);
-
-    return {
-      ...store.state,
-    };
-  },
 };
 </script>
 
--- src/store/index.js
+++ src/store/index.js
@@ -2,7 +2,7 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
-    titleApp: "Memes v2",
+    titleApp: "Memes",
     memes: []
   },
   mutations: {
